02b8b1f89d66211902ef0c540bb028e62e1316c4
Philippe Arteau <parteau@gosecure.ca>
2016-03-21 17:42:41 -0400

    Minor refactoring and icon change

--- js/controllers.js
+++ js/controllers.js
@@ -5,9 +5,16 @@
 var app = angular.module('checklistApp', []);
 
 app.controller('SecurityChecklistCtrl', function($scope, $http, $sce) {
+
+    //////////////////////////
+    // Model
+    //////////////////////////
+
+    //Model exposed to the view
     $scope.filter      = {}; //Selected frameworks
     $scope.guidelines  = []; //Contains the descriptions and content for each rules
 
+    //Load a single guideline
     $scope.addGuideline = function (title, tag, descriptionHtml, ref) {
         $scope.guidelines.push(
             {"title": title,
@@ -17,24 +24,37 @@ app.controller('SecurityChecklistCtrl', function($scope, $http, $sce) {
         );
     };
 
+    //////////////////////////
+    // Data loading
+    //////////////////////////
+
+
     //Parsing that allow Multine string in JSON. It make it easier for maintenance
     function jsonSpecialParse(data) {
         return angular.fromJson(data.replace(/[\r\n]+/g, ''));
     }
 
-    //Loading guildelines
-    $http.get('data/guildelines.json',
-        {transformResponse: function(d, h) {
-            return jsonSpecialParse(d);
-        }})
-        .success(function (data) {
+    function loadJson(data) {
         //console.info(data);
         for (var i = 0; i < data.length; i++) {
             var item = data[i];
             //console.info(item);
             $scope.addGuideline(item[0], item[1], item[2], item[3]);
         }
-    });
+    }
+
+    //Loading guildelines
+    var transformers = {transformResponse: function(d, h) { return jsonSpecialParse(d); }}
+
+    $http.get('data/dot_net.json',transformers).success(loadJson);
+    $http.get('data/javascript.json',transformers).success(loadJson);
+
+
+
+    //////////////////////////
+    // Filtering
+    //////////////////////////
+
 
     function noFilter(filterObj) {
         for (var key in filterObj) {
